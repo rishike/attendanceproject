12680874a7bea2f90c3faf2f2968c3595f1a94f5
rishike <rishisingh.singh3@gmail.com>
2021-08-08 13:00:05 +0530

    heroku server changes

--- attendanceProject/settings.py
+++ attendanceProject/settings.py
@@ -120,8 +120,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'public'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "capture")
